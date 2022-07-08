649b70ef1940f530ab150813329d87fece564693
journo10 <akifbgtr@gmail.com>
2022-07-08 23:49:12 +0300

    route ve model

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,13 +1,38 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./src/routes/auth.js";
+import usersRoute from "./src/routes/users.js";
+import hotelsRoute from "./src/routes/hotels.js";
+import roomsRoute from "./src/routes/rooms.js";
 
 
 
 const app = express();
+dotenv.config();
 
-app.listen(8800, () =>{
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to mongoDB!!!");
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
+
+//middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+
+app.listen(8080, () => {
+    connect()
     console.log("Connected to backend!")
 })
-
-
-
-
