54badae670637b6e33228a41f6cd7cc7af3b527c
journo10 <akifbgtr@gmail.com>
2022-07-12 23:23:19 +0300

    nodejs kısmı bitti

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,7 +5,8 @@ import authRoute from "./src/routes/auth.js";
 import usersRoute from "./src/routes/users.js";
 import hotelsRoute from "./src/routes/hotels.js";
 import roomsRoute from "./src/routes/rooms.js";
-
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 
 const app = express();
@@ -14,7 +15,7 @@ dotenv.config();
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO);
         console.log("Connected to mongoDB!!!");
     } catch (error) {
         throw error;
@@ -26,10 +27,26 @@ mongoose.connection.on("disconnected", () => {
 });
 
 //middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
 
 
 app.listen(8080, () => {
